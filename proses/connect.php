9c51a88baef4efd511d42a50cb00b79d1142205b
Muhammad <momolhayabusa@gmail.com>
2023-10-30 07:42:33 +0700

    perubahan connect

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "db_decafe");
+$conn = mysqli_connect("localhost", "u837952942_muhammadmaula", "g8U!aX>W$", "u837952942_muhammadmaula");
 if (!$conn) {
     echo "Gagal terkoneksi";
 }
